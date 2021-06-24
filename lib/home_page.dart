559a2a9dce7d50a03172c83a0b9a32fe948b9e8e
Rushikesh Welkar <rushikesh.welkar@nciportal.com>
2021-06-24 17:18:11 +0530

    initial

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -6,7 +6,9 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+    
+      ),
       body: const Center
       (child: 
       Text('Welcome to flutter')
